96365dc09528142ee6b6f5c07392bfd1a4a95575
Vikash8460 <Vikashkamat8080@gmail.com>
2024-05-07 15:46:20 +0530

    write the code in hero.js

---
+++ react-coffee-app/src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/Hero.css";
+
+function Hero() {
+  return (
+    <>
+      <section className="hero">
+        <div className="container">
+          <h1>
+            What is your <br /> favourite taste?
+          </h1>
+          <div className="hero-link">
+            <a href="/types" className="explore">
+              Choose
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;

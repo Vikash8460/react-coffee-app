a41199a339f30d20028686754f181b6d13b4488f
Vikash8460 <Vikashkamat8080@gmail.com>
2024-05-07 15:48:20 +0530

    write the code in navbar.js

---
+++ react-coffee-app/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import "../css/Navbar.css";
+
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <nav className="sticky">
+        <a href="/" className="coffee-title">
+          CoffeeMe
+        </a>
+        <div className={showMenu ? "menu mobile-menu" : "menu"}>
+          <ul>
+            <li>
+              <a href="/"> Home </a>
+            </li>
+            <li>
+              <a href="/history"> Hystory of coffee </a>
+            </li>
+            <li>
+              <a href="/types"> Coffee types </a>
+            </li>
+            <li>
+              <a href="/benefits"> Coffee benefits </a>
+            </li>
+          </ul>
+        </div>
+        <FaBars className="bar" onClick={() => setShowMenu(!showMenu)} />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;

5e0d62d5f3fd95280f06d57be62cb20f96e06842
Vikash8460 <Vikashkamat8080@gmail.com>
2024-05-07 15:54:50 +0530

    write the code in home.js

---
+++ react-coffee-app/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Counter from "../components/Counter";
+import Hero from "../components/Hero";
+import Sections from "../components/Sections";
+import Slider from "../components/Slider";
+import Video from "../components/Video";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Counter />
+      <Sections />
+      <Slider />
+      <Video />
+    </div>
+  );
+}
+
+export default Home;

361addff2a32c8d8a04fb9ecc24be3996c5335f0
Vikash8460 <Vikashkamat8080@gmail.com>
2024-05-07 15:44:36 +0530

    write the code in buttons.js

---
+++ react-coffee-app/src/components/Buttons.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Buttons({ chooseType }) {
+  return (
+    <div className="btn-container">
+      <button className="change" onClick={() => chooseType("hot")}>
+        hot coffee
+      </button>
+      <button className="change" onClick={() => chooseType("espresso")}>
+        espresso
+      </button>
+      <button className="change" onClick={() => chooseType("iced")}>
+        iced coffee
+      </button>
+      <button className="change" onClick={() => window.location.reload(false)}>
+        all types
+      </button>
+    </div>
+  );
+}
+
+export default Buttons;
